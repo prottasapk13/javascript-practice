a3f27e9b1ab14689495b64933fbbc81df3a34940
Prottasa Chanda <prottasapk13@gmail.com>
2026-08-11 20:44:12 +0600

    Add JavaScript practice exercises 4  to 6

---
+++ exercise3.js
@@ -0,0 +1,5 @@
+//remove a quotation mark from a console.log line
+console.log ("JavaScript);
+
+//After putting a quotation mark
+console.log ("JavaScript");
---
+++ exercise4.js
@@ -0,0 +1,5 @@
+const name = "Prottasa";
+const department = "CSE";
+let semester = "8th";
+
+console.log(`My name is ${name}, I study ${department}, and I am in ${semester} semester.`);
