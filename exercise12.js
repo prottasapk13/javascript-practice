9da8b201700813a51e06576a407a980129c7bd67
Prottasa Chanda <prottasapk13@gmail.com>
2026-08-13 00:17:29 +0600

    Add JavaScript loop practice exercises 11 to 15

---
+++ exercise12.js
@@ -0,0 +1,12 @@
+let sum = 0;
+
+for(let i =1 ; i<= 100; i++ ){
+
+    sum += i;
+}
+
+let n = 100;
+let formula = (n * (n + 1 )) / 2;
+
+console.log("Loop Sum:",sum);
+console.log ("Formula Sum:",formula);
